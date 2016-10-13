d40e47289b1e008d4c46cf7f3a61c01befba415a
declantyson <declantyson@hotmail.co.uk>
2016-10-13 12:28:58 +0100

    Prepare recipes

--- new-project.sh
+++ new-project.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-default=~/workspace/schnell/default
+base=~/workspace/schnell/recipes/
 prefix=~/dev-dt/
 
 if [ $# -lt 1 ]; then
     echo -n 'Enter project name:'
     read project
 fi
+
 while test $# -gt 0; do
     case "$1" in
         -h|--help)
@@ -32,45 +33,25 @@ while test $# -gt 0; do
     esac
 done
 
+echo -n 'Choose recipe:'
+read recipe
+
+if [ ! -d recipes/$recipe ]; then
+    echo "Error: cannot find recipe '$recipe'."
+    exit 0
+fi
+
+if [ -z $recipe ]; then
+    recipe="default"
+fi
+
 echo "Creating new project '$project'"
 
 projectdir=$prefix$project
+recipedir=$base/$recipe
 
 mkdir $projectdir
-
-mkdir $projectdir/app
-mkdir $projectdir/app/_css
-mkdir $projectdir/app/_scripts
-mkdir $projectdir/app/_libs
-mkdir $projectdir/app/_data
-mkdir $projectdir/app/_tests
-mkdir $projectdir/app/_images
-
-cp $default/run.sh $projectdir/run.sh
-cp $default/Gruntfile.js $projectdir/Gruntfile.js
-cp $default/index.ejs $projectdir/app/index.ejs
-cp $default/server.js $projectdir/server.js
-cp $default/test-spec.js $projectdir/app/_tests/test-spec.js
-cp -r $default/_scripts/ $projectdir/app/_scripts/
-cp -r $default/_css/ $projectdir/app/_css/
+bash recipes/$recipe/install.sh $projectdir $recipedir
 
 cd $projectdir
-npm init --yes
-npm install grunt --save-dev
-npm install grunt-contrib-sass --save-dev
-npm install grunt-contrib-uglify --save-dev
-npm install grunt-contrib-cssmin --save-dev
-npm install grunt-contrib-watch --save-dev
-npm install grunt-contrib-connect --save-dev
-npm install grunt-contrib-jasmine --save-dev
-npm install grunt-contrib-jshint --save-dev
-npm install grunt-notify --save-dev
-
-npm install body-parser --save-dev
-npm install ejs --save-dev
-npm install express --save-dev
-npm install fs --save-dev
-
-# sudo npm install -g ttab
 bash run.sh
-cd app
